dbe124287af6a7fbbf8dcb8e780710f8c945db3e
sagar097 <spawar097@gmail.com>
2019-04-09 15:37:04 +0530

    build changes

--- src/components/project/React.js
+++ src/components/project/React.js
@@ -17,7 +17,7 @@ import ShareIcon from '@material-ui/icons/Share';
 import classnames from 'classnames';
 import red from '@material-ui/core/colors/red';
 import weather  from '../../static/images/weather.jpg';
-import website  from '../../static/images/website.jpg';
+import website  from '../../static/images/Website.JPG';
 
 const styles = theme => ({
   card: {
